e157a59ddc19e5842b9d3de56ceb450385558d6d
Kassian Wren <the@nodebotani.st>
2021-08-28 05:20:05 -0500

    Added prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "prettier"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "semi": ["off", "always"]
+    }
+};
